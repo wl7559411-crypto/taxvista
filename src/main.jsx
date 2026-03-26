eadfdbdfd001fdcd5617e830653c3b387b657782
wl7559411-crypto <wl7559411-crypto@users.noreply.github.com>
2026-03-25 23:26:30 -0400

    add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
+import { inject } from '@vercel/analytics'
+inject()
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
